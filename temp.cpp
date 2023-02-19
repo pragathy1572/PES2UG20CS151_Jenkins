240526934aeb9bd294bb4f81a75fabacf76f0197
pragathy1572 <92253848+pragathy1572@users.noreply.github.com>
2023-02-19 12:21:50 +0530

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+  cout << "Hello, World this is KN Pragathy Reddy\n";
+  cout << " I am in task5 my SRN is PES2UG20CS151\n";
+  return 0;
+}
